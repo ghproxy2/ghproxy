bf46b6547953359e5c47d15ef9308d7a6c5183e7
ghproxy2 <138092713+ghproxy2@users.noreply.github.com>
2023-06-29 13:01:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// const express = require('express');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// const app = express();
+// app.use('/', createProxyMiddleware({ target: 'https://github.com/', changeOrigin: true }));
+// app.listen(3000);
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-app.use('/', createProxyMiddleware({ target: 'https://github.com/', changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: 'https://www.google.com/', changeOrigin: true }));
 app.listen(3000);
