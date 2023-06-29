0f7c217515e2118c2075b3b960250823cf66efa0
ghproxy2 <138092713+ghproxy2@users.noreply.github.com>
2023-06-29 13:18:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-app.use('/', createProxyMiddleware({ target: 'https://www.google.com/', changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: 'https://www.youtube.com/', changeOrigin: true }));
 app.listen(3000);
